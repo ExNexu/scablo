105b40395797cdbdf8e00fc1d4fe31c0e42ffc93
ExNexu <stefan.bleibinhaus@gmail.com>
2013-06-10 13:17:30 +0200

    added todos in tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -6,10 +6,10 @@ import play.api.test._
 import play.api.test.Helpers._
 
 /**
-  * Add your spec here.
-  * You can mock out a whole application including requests, plugins etc.
-  * For more information, consult the wiki.
-  */
+ * TODO: Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
 class ApplicationSpec extends Specification {
 
   "Application" should {
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -6,9 +6,9 @@ import play.api.test._
 import play.api.test.Helpers._
 
 /**
-  * add your integration spec here.
-  * An integration test will fire up a whole play application in a real (or headless) browser
-  */
+ * TODO: add your integration spec here.
+ * An integration test will fire up a whole play application in a real (or headless) browser
+ */
 class IntegrationSpec extends Specification {
 
   "Application" should {
