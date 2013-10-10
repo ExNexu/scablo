13508af75d7c24aed8b9b8cb47b57466914aabf0
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2013-10-10 21:46:59 +1300

    fixed tests

--- test/backend/engine/TextCompilerSbtlTest.scala
+++ test/backend/engine/TextCompilerSbtlTest.scala
@@ -22,14 +22,7 @@ class TextCompilerSbtlTest extends Specification {
 </sbtl>""")
 
         encoded._1.trim() must equalTo("abstract")
-        encoded._2.trim() must equalTo("""<div class="abstract">
-    abstract
-  </div><hr /><div class="contents"><h4>Contents</h4><ul><li class="h1"><a href="#header-1" class="l1header">
-            1 header
-          </a></li></ul></div><hr /><div class="text">
-    <h4><a name="header-1"></a> 1 header</h4>
-    <p>text</p>
-  </div>""")
+        encoded._2.trim() must equalTo("""<div class="abstract"> abstract </div><hr /><div class="contents"><h4>Contents</h4><ul><li class="h1"><a href="#header-1" class="l1header"> 1 header </a></li></ul></div><hr /><div class="text"> <h4><a name="header-1"></a> 1 header</h4> <p>text</p> </div>""")
       }
     }
     "be able to compile a complex sbtl text" in {
@@ -59,85 +52,8 @@ class TextCompilerSbtlTest extends Specification {
   </text>
 </sbtl>""")
 
-        encoded._1.trim() must equalTo("""abstract<b>b</b><i>i</i><b><i>bi</i></b><a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank">
-              la<b>b</b><i>i</i><b><i>bi</i></b>
-              <i class="icon-external-link"></i>
-            </a>la<b>b</b><i>i</i><b><i>bi</i></b>""")
-        encoded._2.trim() must equalTo("""<div class="abstract">
-    abstract<b>b</b><i>i</i><b><i>bi</i></b><a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank">
-          la<b>b</b><i>i</i><b><i>bi</i></b>[1]<i class="icon-external-link"></i>
-        </a><a href="#ref-2" title="sad by au">la<b>b</b><i>i</i><b><i>bi</i></b> [2]</a>
-  </div><hr /><div class="contents"><h4>Contents</h4><ul><li class="h1"><a href="#header-1" class="l1header">
-            1 header1
-          </a></li><li class="h1"><a href="#h2" class="l1header">
-            2 header2
-          </a></li><ul><li class="h2"><a href="#h21" class="l2header">
-                2.1 header21
-              </a></li></ul><li class="h1"><a href="#header-3" class="l1header">
-            3 References
-          </a></li></ul></div><hr /><div class="text">
-    <h4><a name="header-1"></a> 1 header1</h4>
-    <p>la<b>b</b><i>i</i><b><i>bi</i></b><a href="#footnote-1"><sup>1</sup></a></p>
-    <h4><a name="h2"></a> 2 header2</h4>
-    <ul>
-      <li><a href="https://github.com/ExNexu/scablo" title="t by a (https://github.com/ExNexu/scablo)" target="_blank">
-          asd<a href="#footnote-3"><sup>3</sup></a>[4]<i class="icon-external-link"></i>
-        </a></li>
-      <li><div class="img"><a href="https://github.com/ExNexu/scablo" target="_blank"><img src="https://github.com/ExNexu/scablo" alt="Image 1" /></a> <div class="imgTitleDescr">Image 1</div></div></li>
-      <li><div class="img"><a name="img"></a><a href="https://github.com/ExNexu/scablo" target="_blank">
-                                                <img src="https://github.com/ExNexu/scablo" alt="Image 2" />
-                                              </a> <div class="imgTitleDescr">Image 2</div></div></li>
-    </ul>
-    <p><a href="#h21" title="2.1 header21"><i>2.1 header21</i></a>-<a href="#img" title="Image 2"><i>Image 2</i></a>-<a href="#img2" title="Image 4"><i>Image 4</i></a>-<a href="#gist" title="Code snippet 1"><i>Code snippet 1</i></a></p>
-    <p><b>b</b><i>i</i><b><i>bi</i></b></p>
-    <h5><a name="h21"></a> 2.1 header21</h5>
-    <div class="img"><a href="https://github.com/ExNexu/scablo" target="_blank"><img src="https://github.com/ExNexu/scablo" alt="Image 3" /></a> <div class="imgTitleDescr">Image 3</div></div>
-    <div class="img"><a name="img2"></a><a href="https://github.com/ExNexu/scablo" target="_blank">
-                                                <img src="https://github.com/ExNexu/scablo" alt="Image 4" />
-                                              </a> <div class="imgTitleDescr">Image 4</div></div>
-    <div class="code">
-      <div class="gistCode">
-        <a name="gist"></a><script src="https://github.com/ExNexu/scablo"></script>
-      </div>
-      <div class="codeTitleDescr">
-        <div class="codeTitleDescr">Code snippet 1</div>
-      </div>
-    </div>
-    <p><b>b</b><i>i</i><b><i>bi</i></b>
-    <a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank">
-          la<b>b</b><i>i</i><b><i>bi</i></b>[1]<i class="icon-external-link"></i>
-        </a><a href="#ref-2" title="sad by au">la<b>b</b><i>i</i><b><i>bi</i></b> [2]</a></p>
-  </div><hr /><div class="footnotes"><p class="footnote">
-                <a name="footnote-1"></a>
-                <sup>1</sup> f<b>b</b><i>i</i><b><i>bi</i></b><a href="#h21" title="2.1 header21"><i>2.1 header21</i></a><a href="#footnote-2"><sup>2</sup></a><a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo" target="_blank">
-          la<b>b</b><i>i</i><b><i>bi</i></b>[3]<i class="icon-external-link"></i>
-        </a>
-              </p><p class="footnote">
-                <a name="footnote-2"></a>
-                <sup>2</sup> f<b>b</b><i>i</i><b><i>bi</i></b><a href="#h2" title="2 header2"><i>2 header2</i></a>
-              </p><p class="footnote">
-                <a name="footnote-3"></a>
-                <sup>3</sup> f<b>b</b><i>i</i><b><i>bi</i></b><a href="#h2" title="2 header2"><i>2 header2</i></a>
-              </p></div><hr /><div class="references">
-      <h4><a name="header-3"></a> 3 References</h4><ul><li class="ref">
-              [1] 
-              <a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank">
-                https://github.com/ExNexu/scablo by au<i class="icon-external-link"></i>
-              </a>
-            </li><li class="ref">
-              <a name="ref-2"></a>[2] sad by au
-            </li><li class="ref">
-              [3] 
-              <a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo" target="_blank">
-                https://github.com/ExNexu/scablo<i class="icon-external-link"></i>
-              </a>
-            </li><li class="ref">
-              [4] 
-              <a href="https://github.com/ExNexu/scablo" title="t by a (https://github.com/ExNexu/scablo)" target="_blank">
-                t by a (https://github.com/ExNexu/scablo)<i class="icon-external-link"></i>
-              </a>
-            </li></ul>
-    </div>""")
+        encoded._1.trim() must equalTo("""abstract<b>b</b><i>i</i><b><i>bi</i></b><a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank"> la<b>b</b><i>i</i><b><i>bi</i></b> <i class="icon-external-link"></i> </a>la<b>b</b><i>i</i><b><i>bi</i></b>""")
+        encoded._2.trim() must equalTo("""<div class="abstract"> abstract<b>b</b><i>i</i><b><i>bi</i></b><a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank"> la<b>b</b><i>i</i><b><i>bi</i></b> [1] <i class="icon-external-link"></i> </a><a href="#ref-2" title="sad by au">la<b>b</b><i>i</i><b><i>bi</i></b> [2]</a> </div><hr /><div class="contents"><h4>Contents</h4><ul><li class="h1"><a href="#header-1" class="l1header"> 1 header1 </a></li><li class="h1"><a href="#h2" class="l1header"> 2 header2 </a></li><ul><li class="h2"><a href="#h21" class="l2header"> 2.1 header21 </a></li></ul><li class="h1"><a href="#header-3" class="l1header"> 3 References </a></li></ul></div><hr /><div class="text"> <h4><a name="header-1"></a> 1 header1</h4> <p>la<b>b</b><i>i</i><b><i>bi</i></b><a href="#footnote-1"><sup>1</sup></a></p> <h4><a name="h2"></a> 2 header2</h4> <ul> <li><a href="https://github.com/ExNexu/scablo" title="t by a (https://github.com/ExNexu/scablo)" target="_blank"> asd<a href="#footnote-3"><sup>3</sup></a> [4] <i class="icon-external-link"></i> </a></li> <li><div class="img"><a href="https://github.com/ExNexu/scablo" target="_blank"><img src="https://github.com/ExNexu/scablo" alt="Image 1" /></a> <div class="imgTitleDescr">Image 1</div></div></li> <li><div class="img"><a name="img"></a><a href="https://github.com/ExNexu/scablo" target="_blank"> <img src="https://github.com/ExNexu/scablo" alt="Image 2" /> </a> <div class="imgTitleDescr">Image 2</div></div></li> </ul> <p><a href="#h21" title="2.1 header21"><i>2.1 header21</i></a>-<a href="#img" title="Image 2"><i>Image 2</i></a>-<a href="#img2" title="Image 4"><i>Image 4</i></a>-<a href="#gist" title="Code snippet 1"><i>Code snippet 1</i></a></p> <p><b>b</b><i>i</i><b><i>bi</i></b></p> <h5><a name="h21"></a> 2.1 header21</h5> <div class="img"><a href="https://github.com/ExNexu/scablo" target="_blank"><img src="https://github.com/ExNexu/scablo" alt="Image 3" /></a> <div class="imgTitleDescr">Image 3</div></div> <div class="img"><a name="img2"></a><a href="https://github.com/ExNexu/scablo" target="_blank"> <img src="https://github.com/ExNexu/scablo" alt="Image 4" /> </a> <div class="imgTitleDescr">Image 4</div></div> <div class="code"> <div class="gistCode"> <a name="gist"></a><script src="https://github.com/ExNexu/scablo"></script> </div> <div class="codeTitleDescr"> <div class="codeTitleDescr">Code snippet 1</div> </div> </div> <p><b>b</b><i>i</i><b><i>bi</i></b> <a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank"> la<b>b</b><i>i</i><b><i>bi</i></b> [1] <i class="icon-external-link"></i> </a><a href="#ref-2" title="sad by au">la<b>b</b><i>i</i><b><i>bi</i></b> [2]</a></p> </div><hr /><div class="footnotes"><p class="footnote"> <a name="footnote-1"></a> <sup>1</sup> f<b>b</b><i>i</i><b><i>bi</i></b><a href="#h21" title="2.1 header21"><i>2.1 header21</i></a><a href="#footnote-2"><sup>2</sup></a><a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo" target="_blank"> la<b>b</b><i>i</i><b><i>bi</i></b> [3] <i class="icon-external-link"></i> </a> </p><p class="footnote"> <a name="footnote-2"></a> <sup>2</sup> f<b>b</b><i>i</i><b><i>bi</i></b><a href="#h2" title="2 header2"><i>2 header2</i></a> </p><p class="footnote"> <a name="footnote-3"></a> <sup>3</sup> f<b>b</b><i>i</i><b><i>bi</i></b><a href="#h2" title="2 header2"><i>2 header2</i></a> </p></div><hr /><div class="references"> <h4><a name="header-3"></a> 3 References</h4><ul><li class="ref"> [1] <a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo by au" target="_blank"> https://github.com/ExNexu/scablo by au<i class="icon-external-link"></i> </a> </li><li class="ref"> <a name="ref-2"></a>[2] sad by au </li><li class="ref"> [3] <a href="https://github.com/ExNexu/scablo" title="https://github.com/ExNexu/scablo" target="_blank"> https://github.com/ExNexu/scablo<i class="icon-external-link"></i> </a> </li><li class="ref"> [4] <a href="https://github.com/ExNexu/scablo" title="t by a (https://github.com/ExNexu/scablo)" target="_blank"> t by a (https://github.com/ExNexu/scablo)<i class="icon-external-link"></i> </a> </li></ul> </div>""")
       }
     }
     "be able to compile a sbtl text with blockquote" in {
@@ -157,31 +73,7 @@ class TextCompilerSbtlTest extends Specification {
 </sbtl>""")
 
         encoded._1.trim() must equalTo("abstract")
-        encoded._2.trim() must equalTo("""<div class="abstract">
-    abstract
-  </div><hr /><div class="contents"><h4>Contents</h4><ul><li class="h1"><a href="#header-1" class="l1header">
-            1 header
-          </a></li><li class="h1"><a href="#header-2" class="l1header">
-            2 References
-          </a></li></ul></div><hr /><div class="text">
-    <h4><a name="header-1"></a> 1 header</h4>
-    <p>some text</p>
-    <blockquote>
-      <p> 
-      Some text!!
-     </p>
-      <small> scablo github by Some author (https://github.com/ExNexu/scablo) <a href="https://github.com/ExNexu/scablo" title="scablo github by Some author (https://github.com/ExNexu/scablo)" target="_blank">
-          [1]<i class="icon-external-link"></i>
-        </a></small>
-    </blockquote>
-  </div><div class="references">
-      <h4><a name="header-2"></a> 2 References</h4><ul><li class="ref">
-              [1] 
-              <a href="https://github.com/ExNexu/scablo" title="scablo github by Some author (https://github.com/ExNexu/scablo)" target="_blank">
-                scablo github by Some author (https://github.com/ExNexu/scablo)<i class="icon-external-link"></i>
-              </a>
-            </li></ul>
-    </div>""")
+        encoded._2.trim() must equalTo("""<div class="abstract"> abstract </div><hr /><div class="contents"><h4>Contents</h4><ul><li class="h1"><a href="#header-1" class="l1header"> 1 header </a></li><li class="h1"><a href="#header-2" class="l1header"> 2 References </a></li></ul></div><hr /><div class="text"> <h4><a name="header-1"></a> 1 header</h4> <p>some text</p> <blockquote> <p> Some text!! </p> <small> scablo github by Some author (https://github.com/ExNexu/scablo) <a href="https://github.com/ExNexu/scablo" title="scablo github by Some author (https://github.com/ExNexu/scablo)" target="_blank"> [1] <i class="icon-external-link"></i> </a></small> </blockquote> </div><div class="references"> <h4><a name="header-2"></a> 2 References</h4><ul><li class="ref"> [1] <a href="https://github.com/ExNexu/scablo" title="scablo github by Some author (https://github.com/ExNexu/scablo)" target="_blank"> scablo github by Some author (https://github.com/ExNexu/scablo)<i class="icon-external-link"></i> </a> </li></ul> </div>""")
       }
     }
   }
